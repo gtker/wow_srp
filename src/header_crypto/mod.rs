05f9a2f45410a274cf0d7548ae3abd233a5ee734
Gtker <github@gtker.com>
2021-07-08 17:59:15 +0200

    Make most Decryptor and Encryptor have default impl

--- src/header_crypto/mod.rs
+++ src/header_crypto/mod.rs
@@ -11,8 +11,6 @@
 //!
 //! [World Packet]: https://wowdev.wiki/World_Packet
 
-use std::io::{Read, Write};
-
 use sha1::{Digest, Sha1};
 
 pub use traits::Decrypter;
@@ -48,61 +46,6 @@ pub struct HeaderCrypto {
 }
 
 impl Encrypter for HeaderCrypto {
-    fn write_encrypted_server_header<W: Write>(
-        &mut self,
-        write: &mut W,
-        size: u16,
-        opcode: u16,
-    ) -> std::io::Result<()> {
-        let buf = self.encrypt_server_header(size, opcode);
-
-        write.write_all(&buf)?;
-
-        Ok(())
-    }
-
-    fn write_encrypted_client_header<W: Write>(
-        &mut self,
-        write: &mut W,
-        size: u16,
-        opcode: u32,
-    ) -> std::io::Result<()> {
-        let buf = self.encrypt_client_header(size, opcode);
-
-        write.write_all(&buf)?;
-
-        Ok(())
-    }
-
-    fn encrypt_server_header(
-        &mut self,
-        size: u16,
-        opcode: u16,
-    ) -> [u8; SERVER_HEADER_LENGTH as usize] {
-        let size = size.to_be_bytes();
-        let opcode = opcode.to_le_bytes();
-
-        let mut header = [size[0], size[1], opcode[0], opcode[1]];
-
-        self.encrypt(&mut header);
-
-        header
-    }
-
-    fn encrypt_client_header(
-        &mut self,
-        size: u16,
-        opcode: u32,
-    ) -> [u8; CLIENT_HEADER_LENGTH as usize] {
-        let size = size.to_be_bytes();
-        let opcode = opcode.to_le_bytes();
-
-        let mut header = [size[0], size[1], opcode[0], opcode[1], opcode[2], opcode[3]];
-        self.encrypt(&mut header);
-
-        header
-    }
-
     fn encrypt(&mut self, data: &mut [u8]) {
         for unencrypted in data {
             // x = (d ^ session_key[index]) + previous_value
@@ -119,50 +62,6 @@ impl Encrypter for HeaderCrypto {
 }
 
 impl Decrypter for HeaderCrypto {
-    fn read_and_decrypt_server_header<R: Read>(
-        &mut self,
-        reader: &mut R,
-    ) -> std::io::Result<ServerHeader> {
-        let mut buf = [0u8; SERVER_HEADER_LENGTH as usize];
-        reader.read_exact(&mut buf)?;
-
-        Ok(self.decrypt_server_header(buf))
-    }
-
-    fn read_and_decrypt_client_header<R: Read>(
-        &mut self,
-        reader: &mut R,
-    ) -> std::io::Result<ClientHeader> {
-        let mut buf = [0u8; CLIENT_HEADER_LENGTH as usize];
-        reader.read_exact(&mut buf)?;
-
-        Ok(self.decrypt_client_header(buf))
-    }
-
-    fn decrypt_server_header(
-        &mut self,
-        mut data: [u8; SERVER_HEADER_LENGTH as usize],
-    ) -> ServerHeader {
-        self.decrypt(&mut data);
-
-        let size = u16::from_be_bytes([data[0], data[1]]);
-        let opcode = u16::from_le_bytes([data[2], data[3]]);
-
-        ServerHeader { size, opcode }
-    }
-
-    fn decrypt_client_header(
-        &mut self,
-        mut data: [u8; CLIENT_HEADER_LENGTH as usize],
-    ) -> ClientHeader {
-        self.decrypt(&mut data);
-
-        let size: u16 = u16::from_be_bytes([data[0], data[1]]);
-        let opcode: u32 = u32::from_le_bytes([data[2], data[3], data[4], data[5]]);
-
-        ClientHeader { size, opcode }
-    }
-
     fn decrypt(&mut self, data: &mut [u8]) {
         for encrypted in data {
             // unencrypted = (encrypted - previous_value) ^ session_key[index]
--- src/header_crypto/traits.rs
+++ src/header_crypto/traits.rs
@@ -13,26 +13,55 @@ pub trait Encrypter {
         write: &mut W,
         size: u16,
         opcode: u16,
-    ) -> std::io::Result<()>;
+    ) -> std::io::Result<()> {
+        let buf = self.encrypt_server_header(size, opcode);
+
+        write.write_all(&buf)?;
+
+        Ok(())
+    }
 
     fn write_encrypted_client_header<W: Write>(
         &mut self,
         write: &mut W,
         size: u16,
         opcode: u32,
-    ) -> std::io::Result<()>;
+    ) -> std::io::Result<()> {
+        let buf = self.encrypt_client_header(size, opcode);
+
+        write.write_all(&buf)?;
+
+        Ok(())
+    }
 
     fn encrypt_server_header(
         &mut self,
         size: u16,
         opcode: u16,
-    ) -> [u8; SERVER_HEADER_LENGTH as usize];
+    ) -> [u8; SERVER_HEADER_LENGTH as usize] {
+        let size = size.to_be_bytes();
+        let opcode = opcode.to_le_bytes();
+
+        let mut header = [size[0], size[1], opcode[0], opcode[1]];
+
+        self.encrypt(&mut header);
+
+        header
+    }
 
     fn encrypt_client_header(
         &mut self,
         size: u16,
         opcode: u32,
-    ) -> [u8; CLIENT_HEADER_LENGTH as usize];
+    ) -> [u8; CLIENT_HEADER_LENGTH as usize] {
+        let size = size.to_be_bytes();
+        let opcode = opcode.to_le_bytes();
+
+        let mut header = [size[0], size[1], opcode[0], opcode[1], opcode[2], opcode[3]];
+        self.encrypt(&mut header);
+
+        header
+    }
 
     fn encrypt(&mut self, data: &mut [u8]);
 }
@@ -41,16 +70,46 @@ pub trait Decrypter {
     fn read_and_decrypt_server_header<R: Read>(
         &mut self,
         reader: &mut R,
-    ) -> std::io::Result<ServerHeader>;
+    ) -> std::io::Result<ServerHeader> {
+        let mut buf = [0u8; SERVER_HEADER_LENGTH as usize];
+        reader.read_exact(&mut buf)?;
+
+        Ok(self.decrypt_server_header(buf))
+    }
 
     fn read_and_decrypt_client_header<R: Read>(
         &mut self,
         reader: &mut R,
-    ) -> std::io::Result<ClientHeader>;
+    ) -> std::io::Result<ClientHeader> {
+        let mut buf = [0u8; CLIENT_HEADER_LENGTH as usize];
+        reader.read_exact(&mut buf)?;
 
-    fn decrypt_server_header(&mut self, data: [u8; SERVER_HEADER_LENGTH as usize]) -> ServerHeader;
+        Ok(self.decrypt_client_header(buf))
+    }
 
-    fn decrypt_client_header(&mut self, data: [u8; CLIENT_HEADER_LENGTH as usize]) -> ClientHeader;
+    fn decrypt_server_header(
+        &mut self,
+        mut data: [u8; SERVER_HEADER_LENGTH as usize],
+    ) -> ServerHeader {
+        self.decrypt(&mut data);
+
+        let size = u16::from_be_bytes([data[0], data[1]]);
+        let opcode = u16::from_le_bytes([data[2], data[3]]);
+
+        ServerHeader { size, opcode }
+    }
+
+    fn decrypt_client_header(
+        &mut self,
+        mut data: [u8; CLIENT_HEADER_LENGTH as usize],
+    ) -> ClientHeader {
+        self.decrypt(&mut data);
+
+        let size: u16 = u16::from_be_bytes([data[0], data[1]]);
+        let opcode: u32 = u32::from_le_bytes([data[2], data[3], data[4], data[5]]);
+
+        ClientHeader { size, opcode }
+    }
 
     fn decrypt(&mut self, data: &mut [u8]);
 }
