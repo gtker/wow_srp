94bf7db96b302f9f1e226fd6cc682dda524690aa
Gtker <git@gtker.com>
2023-09-12 19:56:39 +0200

    Add tests for incorrect proofs

--- src/test.rs
+++ src/test.rs
@@ -13,6 +13,7 @@ fn authenticate_with_self() {
     let password: NormalizedString = NormalizedString::new("A").unwrap();
     let verifier = SrpVerifier::from_username_and_password(username, password);
 
+    assert_eq!(verifier.username(), "A");
     let password_verifier = *verifier.password_verifier();
     let client_salt = *verifier.salt();
 
@@ -81,4 +82,90 @@ fn authenticate_with_self() {
         .verify_reconnection_attempt(reconnection_data.challenge_data, reconnection_data.proof);
 
     assert!(verified);
+}
+
+#[test]
+fn verify_server_proof_incorrect() {
+    let username: NormalizedString = NormalizedString::new("A").unwrap();
+    let password: NormalizedString = NormalizedString::new("A").unwrap();
+    let client = SrpClientUser::new(username, password);
+
+    let username: NormalizedString = NormalizedString::new("A").unwrap();
+    let password: NormalizedString = NormalizedString::new("A").unwrap();
+    let verifier = SrpVerifier::from_username_and_password(username, password);
+
+    let password_verifier = *verifier.password_verifier();
+    let client_salt = *verifier.salt();
+
+    let server = verifier.into_proof();
+
+    let server_salt = *server.salt();
+    let server_public_key = *server.server_public_key();
+
+    let client = client.into_challenge(
+        GENERATOR,
+        LARGE_SAFE_PRIME_LITTLE_ENDIAN,
+        PublicKey::from_le_bytes(*server.server_public_key()).unwrap(),
+        *server.salt(),
+    );
+    let client_public_key = *client.client_public_key();
+
+    let (_, mut server_proof) = match server.into_server(
+        PublicKey::from_le_bytes(client_public_key).unwrap(),
+        *client.client_proof(),
+    ) {
+        Ok(s) => s,
+        Err(e) => {
+            panic!(
+                "'{}'\
+                \nverifier: {:02x?}\
+                \nclient_salt: {:02x?}\
+                \nserver_salt: {:02x?}\
+                \nserver_public_key: {:02x?}\
+                \nclient_public_key: {:02x?}",
+                e,
+                password_verifier,
+                client_salt,
+                server_salt,
+                server_public_key,
+                client_public_key,
+            )
+        }
+    };
+
+    server_proof[0] = server_proof[0].wrapping_add(1);
+
+    assert!(client.verify_server_proof(server_proof).is_err());
+}
+
+#[test]
+fn verify_client_proof_incorrect() {
+    let username: NormalizedString = NormalizedString::new("A").unwrap();
+    let password: NormalizedString = NormalizedString::new("A").unwrap();
+    let client = SrpClientUser::new(username, password);
+
+    let username: NormalizedString = NormalizedString::new("A").unwrap();
+    let password: NormalizedString = NormalizedString::new("A").unwrap();
+    let verifier = SrpVerifier::from_username_and_password(username, password);
+
+    let server = verifier.into_proof();
+
+    let client = client.into_challenge(
+        GENERATOR,
+        LARGE_SAFE_PRIME_LITTLE_ENDIAN,
+        PublicKey::from_le_bytes(*server.server_public_key()).unwrap(),
+        *server.salt(),
+    );
+    let client_public_key = *client.client_public_key();
+
+    let mut client_proof = *client.client_proof();
+
+    client_proof[0] = client_proof[0].wrapping_add(1);
+
+    assert!(server
+        .into_server(
+            PublicKey::from_le_bytes(client_public_key).unwrap(),
+            client_proof,
+        )
+        .is_err());
 }
